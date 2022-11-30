189878a31ef2ebe8b4188e301a5dfe9c1b0f36d0
ashleylam30 <ashley.lam23@sitechhs.com>
2022-11-30 13:16:34 -0500

    kengklsndksndkg

---
+++ index.js
@@ -0,0 +1,8 @@
+const DOMSelectors = {
+  form: document.getElementById("form"),
+  button: document.getElementsByClassName("btn"),
+  url: document.getElementById("url"),
+  artist: document.getElementById("artist"),
+  display: document.getElementById("display"),
+  title: document.getElementById("title"),
+};
