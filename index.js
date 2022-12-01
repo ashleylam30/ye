37098b8dbe5c6188417352f6765f5241b7039c16
ashleylam30 <ashley.lam23@sitechhs.com>
2022-12-01 12:25:49 -0500

    meowmemowmeowkmoeowmworqaf
    yjrjrsjy

--- index.js
+++ index.js
@@ -6,3 +6,23 @@ const DOMSelectors = {
   display: document.getElementById("display"),
   title: document.getElementById("title"),
 };
+
+function displayInput() {
+  const userInput = DOMSelectors.input.value;
+  const text = document.createElement("p");
+  text.innerText = userInput;
+  document.body.appendChild(text);
+}
+function clearInput() {
+  DOMSelectors.input.value = "";
+}
+
+function createElement() {
+  displayInput();
+  clearInput();
+  removeObject();
+}
+function removeObject() {
+  const remove = DOMSelectors.input.value;
+  remove.remove();
+}
